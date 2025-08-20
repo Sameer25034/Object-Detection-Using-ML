b062871a950b68eaf46f8e50923e0d867a27778e
Sameer Singh <2k22.aiml.2212254@gmail.com>
2025-08-20 21:34:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // 🚀 Restrict Mobile Access
 function checkDevice() {
-    if (window.innerWidth < 768) {
-        document.body.innerHTML = `
+  if (window.innerWidth < 768) {
+    document.body.innerHTML = `
             <div style="text-align:center; padding: 50px;">
                 <h2>📵 Not Accessible on Mobile</h2>
                 <p>Please use a PC or Tablet to access this site.</p>
             </div>`;
-    }
+  }
 }
 
 // Run on Load
@@ -15,99 +15,104 @@ window.onload = checkDevice;
 // Also check on Resize
 window.onresize = checkDevice;
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Smooth scrolling for navigation links
+  document.querySelectorAll("nav a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetSection = document.querySelector(this.getAttribute("href"));
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Smooth scrolling for navigation links
-    document.querySelectorAll('nav a').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetSection = document.querySelector(this.getAttribute('href'));
-            targetSection.scrollIntoView({
-                behavior: 'smooth'
-            });
-
-            // Highlight the current section in the navigation bar
-            document.querySelectorAll('nav a').forEach(link => link.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });document.addEventListener('DOMContentLoaded', function () {
-    const mobileInput = document.getElementById('mobile-number');
+      // Highlight the current section in the navigation bar
+      document
+        .querySelectorAll("nav a")
+        .forEach((link) => link.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    const mobileInput = document.getElementById("mobile-number");
 
     if (mobileInput) {
-        mobileInput.addEventListener('input', function (e) {
-            this.value = this.value.replace(/[^0-9+]/g , ''); // Remove anything that's not a number or a plus sign
-        });
+      mobileInput.addEventListener("input", function (e) {
+        this.value = this.value.replace(/[^0-9+]/g, ""); // Remove anything that's not a number or a plus sign
+      });
     } else {
-        console.error("Error: #mobile-number element not found in DOM!");
+      console.error("Error: #mobile-number element not found in DOM!");
     }
-});
-document.addEventListener('DOMContentLoaded', function () {
-    const countryCode = document.getElementById('country-code');
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    const countryCode = document.getElementById("country-code");
 
     if (countryCode) {
-        countryCode.style.marginRight = "10px";  // Adds space between country code and input field
+      countryCode.style.marginRight = "10px"; // Adds space between country code and input field
     } else {
-        console.error("Error: #country-code element not found in DOM!");
+      console.error("Error: #country-code element not found in DOM!");
     }
-});
+  });
 
-    // Apply dark mode on page load if user preference is stored
-    const savedDarkMode = localStorage.getItem("darkMode");
-    if (savedDarkMode === "true") {
-        toggleDarkMode();
-    }
+  // Apply dark mode on page load if user preference is stored
+  const savedDarkMode = localStorage.getItem("darkMode");
+  if (savedDarkMode === "true") {
+    toggleDarkMode();
+  }
 
-    // Add an event listener to the dark mode navigation link
-    const darkModeNavLink = document.querySelector('#darkModeNavLink');
-    if (darkModeNavLink) {
-        darkModeNavLink.addEventListener('click', function (event) {
-            event.preventDefault();
-            toggleDarkMode();
-        });
-    }
+  // Add an event listener to the dark mode navigation link
+  const darkModeNavLink = document.querySelector("#darkModeNavLink");
+  if (darkModeNavLink) {
+    darkModeNavLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      toggleDarkMode();
+    });
+  }
 });
 
 var videoStream; // To store the video stream reference
-var demoWindow;  // To store the reference to the demo window
+var demoWindow; // To store the reference to the demo window
 
 function toggleSection(sectionId) {
-    // Get all sections
-    const sections = document.querySelectorAll('section');
-    // Loop through each section and hide them all
-    sections.forEach(section => {
-        section.style.display = 'none';
-    });
+  // Get all sections
+  const sections = document.querySelectorAll("section");
+  // Loop through each section and hide them all
+  sections.forEach((section) => {
+    section.style.display = "none";
+  });
 
-    // Show only the clicked section
-    const selectedSection = document.getElementById(sectionId);
-    if (selectedSection) {
-        selectedSection.style.display = 'block';
-    }
+  // Show only the clicked section
+  const selectedSection = document.getElementById(sectionId);
+  if (selectedSection) {
+    selectedSection.style.display = "block";
+  }
 }
 
 function launchDemo() {
-    // Attempt to access the camera and open the demo window
-    alert('Attempting to access the camera. Please check for any browser prompts.');
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
-            alert('Camera access granted. Opening demo window.');
-            videoStream = stream;
-            openDemoWindow();
+  // Attempt to access the camera and open the demo window
+  alert(
+    "Attempting to access the camera. Please check for any browser prompts."
+  );
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      alert("Camera access granted. Opening demo window.");
+      videoStream = stream;
+      openDemoWindow();
 
-            // Display the webcam feed in the video element
-            const videoElement = demoWindow.document.getElementById('webcamVideo');
-            videoElement.srcObject = stream;
-        })
-        .catch((error) => {
-            console.error('Error accessing camera:', error);
-            alert('Error accessing camera. Check console for details.');
-        });
+      // Display the webcam feed in the video element
+      const videoElement = demoWindow.document.getElementById("webcamVideo");
+      videoElement.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error("Error accessing camera:", error);
+      alert("Error accessing camera. Check console for details.");
+    });
 }
 
 function openDemoWindow() {
-    // Open the demo window and set up its content
-    demoWindow = window.open('', 'DemoWindow', 'width=700,height=600');
-    demoWindow.document.write(`
+  // Open the demo window and set up its content
+  demoWindow = window.open("", "DemoWindow", "width=700,height=600");
+  demoWindow.document.write(`
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -136,49 +141,51 @@ function openDemoWindow() {
 }
 
 function closeDemo() {
-    // Close the demo window and stop the video stream
-    if (videoStream) {
-        videoStream.getTracks().forEach(track => track.stop());
-    }
+  // Close the demo window and stop the video stream
+  if (videoStream) {
+    videoStream.getTracks().forEach((track) => track.stop());
+  }
 
-    if (demoWindow) {
-        demoWindow.close();
-    }
+  if (demoWindow) {
+    demoWindow.close();
+  }
 }
 
 function toggleScrollToTopButton() {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
-    }
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
 }
 
 // Add a function to toggle dark mode
 function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem("darkMode", document.body.classList.contains('dark-mode')); // Save preference
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem(
+    "darkMode",
+    document.body.classList.contains("dark-mode")
+  ); // Save preference
 }
 
-var video = document.getElementById('demoVideo');
-var muteButton = document.getElementById('muteButton');
+var video = document.getElementById("demoVideo");
+var muteButton = document.getElementById("muteButton");
 
 function toggleMute() {
-    if (video) {
-        video.muted = !video.muted;
-        muteButton.textContent = video.muted ? 'Unmute' : 'Mute';
-    }
+  if (video) {
+    video.muted = !video.muted;
+    muteButton.textContent = video.muted ? "Unmute" : "Mute";
+  }
 }
 function tryObjectDetection() {
-    console.log("Opening object detection in a new tab...");
-    window.open("https://object-detection-4u.streamlit.app/", "_blank");
+  console.log("Opening object detection in a new tab...");
+  window.open("https://object-detection-4u.streamlit.app/", "_blank");
 }
 
 function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth' // Smooth scrolling
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Smooth scrolling
+  });
 }
-
